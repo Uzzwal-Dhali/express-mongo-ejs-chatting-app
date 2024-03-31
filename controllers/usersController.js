ab90196621c70e4ea979734d5c5f62ded8159d45
Uzzwal-Dhali <d.uzzwal09@gmail.com>
2024-04-01 01:05:20 +0600

    User deletion is added

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,13 @@
 import bcrypt from "bcrypt"
 
 import User from "../models/People.js"
+import { unlink } from "fs"
+import path from "path"
+import { dirname } from "path"
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 function getRegisterView(req, res, next) {
   res.render("register")
@@ -39,7 +46,6 @@ async function addUser(req, res, next) {
     res.status(200).json({
       message: "User added successfull!"
     })
-    .render("users")
   } catch(err) {
     res.status(500).json({
       errors: {
@@ -51,4 +57,33 @@ async function addUser(req, res, next) {
   }
 }
 
-export { addUser, getRegisterView, getUsers }
+async function deleteUser(req, res, next) {
+  try {
+    const user = await User.findByIdAndDelete({
+      _id: req.params.id,
+    })
+
+    if(user.avatar) {
+      unlink (
+        path.join(__dirname, `/../public/uploads/avatars/${ user.avatar }`),
+        (err) => {
+          if(err) console.log(err)
+        }
+      )
+    }
+
+    res.status(200).json({
+      message: "User deleted successfully!"
+    })
+  } catch (error) {
+    res.status(500).json({
+      errors: {
+        common: {
+          msg: "Oops! Could not delete!",
+        }
+      }
+    })
+  }
+}
+
+export { addUser, getRegisterView, getUsers, deleteUser }
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { check } from "express-validator"
 
-import { addUser, getRegisterView, getUsers } from "../controllers/usersController.js"
+import { addUser, deleteUser, getRegisterView, getUsers } from "../controllers/usersController.js"
 import decorateHtmlResponses from "../middlewares/common/decorateHtmlResponses.js"
 import avatarUpload from "../middlewares/users/avatarUpload.js"
 import { userValidation, userValidationHandler } from "../middlewares/users/userValidation.js"
@@ -12,5 +12,6 @@ router.get("/", decorateHtmlResponses("Users"), getUsers)
 router.post("/")
 router.get("/register", decorateHtmlResponses("Register"), getRegisterView)
 router.post("/register", avatarUpload, userValidation, userValidationHandler, addUser)
+router.delete("/:id", deleteUser)
 
 export default router
